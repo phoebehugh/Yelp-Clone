31501da7f448246ef91c39780a39864ccb4bf52f
phoebehugh <phoebehugh@gmail.com>
2015-08-01 13:17:57 +0100

    Adds restaurant model

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
 feature 'restaurants'  do
-	context 'no restaurants have been added' do
+	context 'No restaurants have been added' do
 		scenario 'should display a prompt to add a restaurant' do
 			visit '/restaurants'
 			expect(page).to have_content 'No restaurants yet'
 			expect(page).to have_link 'Add a restaurant'
+		end
+	end
+
+	context 'Restaurants have been added' do
+		before do
+			Restaurant.create(name: 'Roka')
+		end
+
+		scenario 'Display restaurants' do
+			visit '/restaurants'
+			expect(page).to have_content('Roka')
+			expect(page).not_to have_content('No restaurants yet')
 		end
 	end
 end
