5cc14ab92d652dee036cd38a1d7595e3c38a20a3
phoebehugh <phoebehugh@gmail.com>
2015-08-02 11:08:51 +0100

    Can click on restaurant as a link

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,22 @@
 class RestaurantsController < ApplicationController
 	def index
+		@restaurants = Restaurant.all
+	end
+
+	def new
+		@restaurant = Restaurant.new
+	end
+
+	def create
+		@restaurant = Restaurant.create(restaurant_params)
+		redirect_to '/restaurants'
+	end
+
+	def restaurant_params
+		params.require(:restaurant).permit(:name)
+	end		
+
+	def show
+		@restaurant = Restaurant.find(params[:id])
 	end
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
-feature 'restaurants'  do
+feature 'Restaurants'  do
 	context 'No restaurants have been added' do
 		scenario 'should display a prompt to add a restaurant' do
 			visit '/restaurants'
 			expect(page).to have_content 'No restaurants yet'
 			expect(page).to have_link 'Add a restaurant'
+		end
+	end
+
+	context 'Creating restaurants' do
+		scenario 'prompts user to fill out a form, then displays the new restaurant' do
+			visit '/restaurants'
+			click_link 'Add a restaurant'
+			fill_in 'Name', with: 'Roka'
+			click_button 'Create restaurant'
+			expect(page).to have_content 'Roka'
+			expect(current_path).to eq '/restaurants'
 		end
 	end
 
@@ -18,6 +29,17 @@ feature 'restaurants'  do
 			visit '/restaurants'
 			expect(page).to have_content('Roka')
 			expect(page).not_to have_content('No restaurants yet')
+		end
+	end
+
+	context 'Viewing restaurants' do
+		let!(:roka){Restaurant.create(name:'Roka')}
+
+		scenario 'lets a user view a restaurant' do
+			visit '/restaurants'
+			click_link 'Roka'
+			expect(page).to have_content 'Roka'
+			expect(current_path).to eq "/restaurants/#{roka.id}"
 		end
 	end
 end
